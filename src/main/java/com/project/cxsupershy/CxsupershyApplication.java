28d925396f9e0e97ca269bcc3f1117055ad8cb53
자연이-깃 <wkdus1999@gmail.com>
2024-09-09 15:22:54 +0900

    initial commit

---
+++ src/main/java/com/project/cxsupershy/CxsupershyApplication.java
@@ -0,0 +1,13 @@
+package com.project.cxsupershy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxsupershyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxsupershyApplication.class, args);
+	}
+
+}
